08708d86a3dc5df9bd446d085cabc8b685cffab1
ao2401043-crypto <ao2401043@gmail.com>
2025-09-25 17:20:53 +0200

    Create config.js

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  token: process.env.TOKEN,         // التوكن من المتغيرات
+  prefix: process.env.PREFIX || "!", // البريفكس (افتراضي ! لو ما حددت)
+  owner: process.env.OWNER_ID        // آيدي مالك البوت
+};
