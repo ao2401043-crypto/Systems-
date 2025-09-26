9e8b959e3d3a48ac058b0cc740e5ba7c9d837423
ao2401043-crypto <ao2401043@gmail.com>
2025-09-26 15:55:35 +0200

    Create anti-delete-roles.js

---
+++ Feedback/Feedback/Feedback/Feedback/General/General/General/General/Giveaway/Giveaway/Giveaway/Logs/Logs/Nadeko/Nadeko/Nadeko/Suggestions/Suggestions/Suggestions/protection/protection/protection/anti-delete-roles.js
@@ -0,0 +1,47 @@
+const { SlashCommandBuilder } = require("discord.js");
+const { Database } = require("st.db");
+const db = new Database("/Json-db/Bots/protectDB.json");
+
+module.exports = {
+    ownersOnly: true,
+    data: new SlashCommandBuilder()
+        .setName('anti-delete-roles')
+        .setDescription('تسطيب نظام الحماية من حذف الرتب')
+        .addStringOption(option =>
+            option
+                .setName('status')
+                .setDescription('الحالة')
+                .setRequired(true)
+                .addChoices(
+                    { name: 'On', value: 'on' },
+                    { name: 'Off', value: 'off' }
+                )
+        )
+        .addIntegerOption(option =>
+            option
+                .setName('limit')
+                .setDescription('العدد المسموح في اليوم')
+                .setRequired(true)
+        ),
+
+    async execute(interaction) {
+        await interaction.deferReply({ ephemeral: false });
+        try {
+            const status = interaction.options.getString('status');
+            const limit = interaction.options.getInteger('limit');
+
+            await db.set(`antideleteroles_status_${interaction.guild.id}`, status);
+            await db.set(`antideleteroles_limit_${interaction.guild.id}`, limit);
+            await db.set(`rolesdelete_users_${interaction.guild.id}`, []);
+
+            return interaction.editReply({
+                content: `**تم بنجاح تعيين نظام الحماية من حذف الرتب \n - تأكد من رفع رتبتي لأعلى رتبة في السيرفر**`
+            });
+        } catch (error) {
+            console.error(error);
+            return interaction.editReply({
+                content: `⚠️ حدث خطأ أثناء تنفيذ الأمر.`
+            });
+        }
+    }
+};
